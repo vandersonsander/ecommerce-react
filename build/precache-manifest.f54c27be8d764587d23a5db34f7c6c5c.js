3cf11244dc300ba61d943b5f3204b61e7b2d8bd0
Vanderson Sander <vanderson_sander@hotmail.com>
2020-10-20 15:06:16 -0300

    Finalizado, Produtos, Produto e Requesto por api

---
+++ build/precache-manifest.f54c27be8d764587d23a5db34f7c6c5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9a72334fc5de326c912d70508a807b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ce56695ca62f231d796",
+    "url": "/static/css/main.2e9781be.chunk.css"
+  },
+  {
+    "revision": "f01100c29c935b7cdf76",
+    "url": "/static/js/2.ee367cf8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.ee367cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ce56695ca62f231d796",
+    "url": "/static/js/main.0a08e714.chunk.js"
+  },
+  {
+    "revision": "3cc9d6562f64fc197f20",
+    "url": "/static/js/runtime-main.fc30dc24.js"
+  }
+]);
